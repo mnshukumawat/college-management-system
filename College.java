397f84f694a8a02eb709f8325e5892f549b5d7ef
Mahesh Kumawat <kumawatmahesh794@gmail.com>
2025-07-24 20:15:12 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.college;
+
+/**
+ *
+ * @author 91998
+ */
+public class College {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
